cb96d85c464f11c2ad76ae13bc5b7e888f679fbb
infomconnectpl <122236661+infomconnectpl@users.noreply.github.com>
2025-11-04 21:32:40 +0100

    Delete makeskyblue_controller.cpp

--- makeskyblue_controller.cpp
+++
@@ -1,9 +0,0 @@
-#include "esphome.h"
-#include "makeskyblue/makeskyblue_controller.h"
-
-MakeSkyBlueController* msb_controller;
-
-void setup() {
-  msb_controller = new MakeSkyBlueController(id(uart_msb));
-  App.register_component(msb_controller);
-}
