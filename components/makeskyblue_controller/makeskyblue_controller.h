ed5cbcb1b038d9482c6d97a19fe7e3ea30145265
infomconnectpl <122236661+infomconnectpl@users.noreply.github.com>
2025-11-04 21:17:12 +0100

    Implement MakeSkyBlueController with UART support
    Added MakeSkyBlueController class with sensor definitions and UART handling.

--- components/makeskyblue_controller/makeskyblue_controller.h
+++ components/makeskyblue_controller/makeskyblue_controller.h
@@ -1,1 +1,29 @@
+#pragma once
+
+#include "esphome.h"
+#include "esphome/components/uart/uart.h"
+
+class MakeSkyBlueController : public Component, public UARTDevice {
+ public:
+  MakeSkyBlueController(UARTComponent *parent) : UARTDevice(parent) {}
+
+  Sensor *batt_voltage = new Sensor();
+  Sensor *batt_current = new Sensor();
+  Sensor *solar_voltage = new Sensor();
+  Sensor *solar_power = new Sensor();
+  Sensor *temperature = new Sensor();
+  Sensor *energy_kwh = new Sensor();
+
+  void setup() override {
+    // inicjalizacja, np. resetowanie buforów
+  }
+
+  void loop() override {
+    // przykład odczytu UART (musisz dopasować do swojego protokołu)
+    uint8_t buf;
+    if (this->uart_->read_byte(&buf)) {
+      // przetwarzanie odebranego bajtu
+    }
+  }
+};
 
